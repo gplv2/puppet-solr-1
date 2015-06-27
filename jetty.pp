5fe32cd9ab3a658350751dca0751a51c3109cc72
Glenn Plas <glenn@byte-consult.be>
2015-06-27 03:26:42 +0200

    add extra packages

--- jetty.pp
+++ jetty.pp
@@ -2,13 +2,36 @@ exec { "apt-get update":
      path => '/usr/bin',
 } ->
 
-package { 'default-jdk':
-     ensure => present,
-} ->
-
 package { "java7-jdk":
      ensure => present,
 } -> 
+
+package { 'openjdk-7-jdk':
+    ensure => present,
+} ->
+
+package { 'libjetty-extra-java':
+    ensure => present,
+} ->
+
+package { 'libtomcat7-java':
+    ensure => present,
+} ->
+
+file { "/usr/java":
+    ensure  =>  directory,
+    owner   =>  'root',
+    group   =>  'root',
+    mode    =>  '0755',
+    require => Package['openjdk-7-jdk'],
+} ->
+
+# Ensure the softlink to default java exists
+file { "/usr/java/default":
+    ensure  =>  'link',
+    target  =>  '/usr/lib/jvm/java-7-openjdk-amd64',
+    require => Package['openjdk-7-jdk'],
+} ->
 
 class { 'solr' :
 }
